74a888b0dadf9fa04a3b2e1fdae60167e283c4cc
Eric Sepulvado <jes@knights.ucf.edu>
2013-10-22 22:08:01 -0400

    Refactor to use Fragments.

--- src/com/es0329/fantasyfeed/MainActivity.java
+++ src/com/es0329/fantasyfeed/MainActivity.java
@@ -1,100 +1,47 @@
 package com.es0329.fantasyfeed;
 
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.image.SmartImageView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
 
 /**
  * @author <a href="https://twitter.com/es0329">Eric</a>
  */
-public class MainActivity extends Activity {
-	private ListView list;
-	private ProgressBar progress;
-	private SmartImageView dataCredit;
-	private StoryAdapter adapter;
-	private Story tempStory;
-	private ArrayList<Story> stories;
+public class MainActivity extends FragmentActivity {
+	private Fragment currentFragment;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		wireViews();
-		checkNewsFeed();
-	}
-
-	private void wireViews() {
-		list = (ListView) findViewById(R.id.list);
-		progress = (ProgressBar) findViewById(R.id.progress);
-		dataCredit = (SmartImageView) findViewById(R.id.dataCredit);
-		dataCredit
-				.setImageUrl("http://a.espncdn.com/i/apis/attribution/powered-by-espn-silver_200.png");
-	}
-
-	private void checkNewsFeed() {
-		RequestParams params = new RequestParams("apikey", getResources()
-				.getString(R.string.api_key));
-		Espn.get("", params, reponseHandler);
-	}
-
-	private JsonHttpResponseHandler reponseHandler = new JsonHttpResponseHandler() {
-
-		@Override
-		public void onSuccess(JSONObject response) {
-			stories = new ArrayList<Story>();
-
-			try {
-				JSONArray headlines = response.getJSONArray("headlines");
-				JSONObject result = new JSONObject();
-				JSONObject links = new JSONObject();
-				JSONObject mobile = new JSONObject();
-
-				for (int i = 0; i < headlines.length(); i++) {
-					tempStory = new Story();
-					result = headlines.getJSONObject(i);
-
-					tempStory.setId(result.optString("id"));
-					tempStory.setTitle(result.optString("title"));
-
-					links = result.optJSONObject("links");
-					mobile = links.getJSONObject("mobile");
-					tempStory.setLink(mobile.optString("href"));
-
-					JSONArray images = result.getJSONArray("images");
-
-					if (!images.isNull(0)) {
-						JSONObject url = images.getJSONObject(0);
-						tempStory.setImageUrl(url.getString("url"));
-					} else {
-						tempStory.setImageUrl("");
-					}
-					stories.add(tempStory);
-				}
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			displayStories();
+		if (savedInstanceState == null) {
+			currentFragment = new MasterFragment();
+		} else {
+			currentFragment = getSupportFragmentManager().getFragment(
+					savedInstanceState, "content");
 		}
-	};
+		setFragment(currentFragment);
+	}
 
-	private void displayStories() {
-		Log.i("STORIES", "" + stories.toString());
-		adapter = new StoryAdapter(getApplication(), R.layout.story, stories);
-		list.setAdapter(adapter);
-		progress.setVisibility(View.GONE);
+	protected void setFragment(Fragment fragment) {
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragmentHolder, currentFragment).commit();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		getSupportFragmentManager().putFragment(outState, "content",
+				currentFragment);
+	}
+
+	@Override
+	public void onAttachedToWindow() {
+		super.onAttachedToWindow();
+		Window window = (Window) getWindow();
+		window.setFormat(PixelFormat.RGBA_8888);
 	}
 }
---
+++ src/com/es0329/fantasyfeed/MasterFragment.java
@@ -0,0 +1,102 @@
+package com.es0329.fantasyfeed;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.image.SmartImageView;
+
+/**
+ * @author <a href="https://twitter.com/es0329">Eric</a>
+ */
+public class MasterFragment extends Fragment {
+	private ListView list;
+	private ProgressBar progress;
+	private SmartImageView dataCredit;
+	private StoryAdapter adapter;
+	private Story tempStory;
+	private ArrayList<Story> stories;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View masterLayout = inflater.inflate(R.layout.master, container, false);
+		wireViews(masterLayout);
+		checkNewsFeed();
+
+		return masterLayout;
+	}
+
+	private void wireViews(View layout) {
+		list = (ListView) layout.findViewById(R.id.list);
+		progress = (ProgressBar) layout.findViewById(R.id.progress);
+		dataCredit = (SmartImageView) layout.findViewById(R.id.dataCredit);
+		dataCredit
+				.setImageUrl("http://a.espncdn.com/i/apis/attribution/powered-by-espn-silver_200.png");
+	}
+
+	private void checkNewsFeed() {
+		RequestParams params = new RequestParams("apikey", getResources()
+				.getString(R.string.api_key));
+		Espn.get("", params, reponseHandler);
+	}
+
+	private JsonHttpResponseHandler reponseHandler = new JsonHttpResponseHandler() {
+
+		@Override
+		public void onSuccess(JSONObject response) {
+			stories = new ArrayList<Story>();
+
+			try {
+				JSONArray headlines = response.getJSONArray("headlines");
+				JSONObject result = new JSONObject();
+				JSONObject links = new JSONObject();
+				JSONObject mobile = new JSONObject();
+
+				for (int i = 0; i < headlines.length(); i++) {
+					tempStory = new Story();
+					result = headlines.getJSONObject(i);
+
+					tempStory.setId(result.optString("id"));
+					tempStory.setTitle(result.optString("title"));
+
+					links = result.optJSONObject("links");
+					mobile = links.getJSONObject("mobile");
+					tempStory.setLink(mobile.optString("href"));
+
+					JSONArray images = result.getJSONArray("images");
+
+					if (!images.isNull(0)) {
+						JSONObject url = images.getJSONObject(0);
+						tempStory.setImageUrl(url.getString("url"));
+					} else {
+						tempStory.setImageUrl("");
+					}
+					stories.add(tempStory);
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			displayStories();
+		}
+	};
+
+	private void displayStories() {
+		// Log.i("STORIES", "" + stories.toString());
+		adapter = new StoryAdapter(getActivity(), R.layout.story, stories);
+		list.setAdapter(adapter);
+		progress.setVisibility(View.GONE);
+	}
+}
