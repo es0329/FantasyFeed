575664f307728ece50089768e76585c6341295aa
Eric Sepulvado <jes@knights.ucf.edu>
2013-10-24 10:49:40 -0400

    Refactor to use AsyncTask instead of async-http-client.jar.
    - Update README and credits.html.

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 Fantasy Feed
 ------------
-Get current Fantasy Football news from ESPN experts, as a native mobile app optimized for handheld Android devices.
+A native Android mobile app optimized for handheld devices to get current Fantasy Football news from ESPN experts.
 
 Credits
 -------
-Developed by [Eric Sepulvado][1].
-ESPN's support for Fantasy Sports in their [Headlines API][2].
-[James Smith][3] for his awesome libraries:
-- [Android Asynchronous Http Client][4], a callback-based HTTP client library for Android.
-- [Android Smart Image View][5], loads images from URLs or Android contacts with caching.
+Developed by [Eric Sepulvado][1] with data provided by ESPN's "[Headlines API][2]."
+Many thanks to [James Smith][3] for his awesome Smart-ImageView library.
 
 [1]: https://twitter.com/es0329 "@es0329"
 [2]: http://developer.espn.com/ "ESPN API"
-[3]: https://twitter.com/loopj "@loopj"
-[4]: https://github.com/loopj/android-async-http "Android Asynchronous Http Client"
-[5]: https://github.com/loopj/android-smart-image-view "Android Smart Image View"
+[3]: https://twitter.com/loopj "James Smith"
--- src/com/es0329/fantasyfeed/MasterFragment.java
+++ src/com/es0329/fantasyfeed/MasterFragment.java
@@ -1,7 +1,18 @@
 package com.es0329.fantasyfeed;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ConnectTimeoutException;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,8 +21,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,9 +35,8 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
 import com.loopj.android.image.SmartImageView;
 
 /**
@@ -49,7 +61,9 @@ public class MasterFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View masterLayout = inflater.inflate(R.layout.master, container, false);
 		wireViews(masterLayout);
-		checkNewsFeed();
+		// checkNewsFeed();
+		CheckNews checkNews = new CheckNews();
+		checkNews.execute();
 
 		return masterLayout;
 	}
@@ -69,52 +83,157 @@ public class MasterFragment extends Fragment {
 		});
 	}
 
-	private void checkNewsFeed() {
-		progress.setVisibility(View.VISIBLE);
-		RequestParams params = new RequestParams("apikey", getResources()
-				.getString(R.string.api_key));
-		Espn.get("", params, reponseHandler);
-	}
+	// private void checkNewsFeed() {
+	// progress.setVisibility(View.VISIBLE);
+	// RequestParams params = new RequestParams("apikey", getResources()
+	// .getString(R.string.api_key));
+	// Espn.get("", params, reponseHandler);
+	// }
 
-	private JsonHttpResponseHandler reponseHandler = new JsonHttpResponseHandler() {
+	// private JsonHttpResponseHandler reponseHandler = new
+	// JsonHttpResponseHandler() {
+	//
+	// @Override
+	// public void onSuccess(JSONObject response) {
+	// stories = new ArrayList<Story>();
+	//
+	// // Log.i("RESPONSE", response);
+	//
+	// try {
+	// JSONArray headlines = response.getJSONArray("headlines");
+	// JSONObject result = new JSONObject();
+	// JSONObject links = new JSONObject();
+	// JSONObject mobile = new JSONObject();
+	//
+	// for (int i = 0; i < headlines.length(); i++) {
+	// tempStory = new Story();
+	// result = headlines.getJSONObject(i);
+	//
+	// tempStory.setId(result.optString("id"));
+	// tempStory.setTitle(result.optString("title"));
+	//
+	// links = result.optJSONObject("links");
+	// mobile = links.getJSONObject("mobile");
+	// tempStory.setLink(mobile.optString("href"));
+	//
+	// JSONArray images = result.getJSONArray("images");
+	//
+	// if (!images.isNull(0)) {
+	// JSONObject url = images.getJSONObject(0);
+	// tempStory.setImageUrl(url.getString("url"));
+	// } else {
+	// tempStory.setImageUrl("");
+	// }
+	// stories.add(tempStory);
+	// }
+	// } catch (JSONException e) {
+	// e.printStackTrace();
+	// }
+	// displayStories();
+	// }
+	// };
+
+	private class CheckNews extends AsyncTask<String, Void, String> {
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpParams asyncParams;
+		HttpGet getPost;
+		String result;
 
 		@Override
-		public void onSuccess(JSONObject response) {
-			stories = new ArrayList<Story>();
+		protected void onPreExecute() {
+			super.onPreExecute();
+			asyncParams = client.getParams();
+			HttpConnectionParams.setConnectionTimeout(asyncParams, 30000);
+			HttpConnectionParams.setSoTimeout(asyncParams, 30000);
+			getPost = new HttpGet(Espn.BASE_URL + "apikey="
+					+ getActivity().getResources().getString(R.string.api_key));
+			progress.setVisibility(View.VISIBLE);
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
 
 			try {
-				JSONArray headlines = response.getJSONArray("headlines");
-				JSONObject result = new JSONObject();
-				JSONObject links = new JSONObject();
-				JSONObject mobile = new JSONObject();
-
-				for (int i = 0; i < headlines.length(); i++) {
-					tempStory = new Story();
-					result = headlines.getJSONObject(i);
-
-					tempStory.setId(result.optString("id"));
-					tempStory.setTitle(result.optString("title"));
-
-					links = result.optJSONObject("links");
-					mobile = links.getJSONObject("mobile");
-					tempStory.setLink(mobile.optString("href"));
-
-					JSONArray images = result.getJSONArray("images");
-
-					if (!images.isNull(0)) {
-						JSONObject url = images.getJSONObject(0);
-						tempStory.setImageUrl(url.getString("url"));
-					} else {
-						tempStory.setImageUrl("");
-					}
-					stories.add(tempStory);
-				}
-			} catch (JSONException e) {
+				Log.i("GET", getPost.getURI().toString());
+				HttpResponse response = client.execute(getPost);
+				result = inputStreamToString(response.getEntity().getContent())
+						.toString();
+			} catch (ConnectTimeoutException e) {
+				Toast.makeText(getActivity(), "No Internet", Toast.LENGTH_SHORT)
+						.show();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			displayStories();
+			return result;
 		}
-	};
+
+		private StringBuilder inputStreamToString(InputStream stream) {
+			String line = "";
+			StringBuilder result = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					stream));
+
+			try {
+				while ((line = reader.readLine()) != null) {
+					result.append(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			try {
+				buildArticleList(result);
+			} catch (Exception E) {
+				Toast.makeText(getActivity(), "Error:" + E.getMessage(),
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+	private void buildArticleList(String rawJson) {
+		stories = new ArrayList<Story>();
+
+		Log.i("RESPONSE", rawJson);
+
+		try {
+			JSONObject response = new JSONObject(rawJson);
+			JSONArray headlines = response.getJSONArray("headlines");
+			JSONObject result = new JSONObject();
+			JSONObject links = new JSONObject();
+			JSONObject mobile = new JSONObject();
+
+			for (int i = 0; i < headlines.length(); i++) {
+				tempStory = new Story();
+				result = headlines.getJSONObject(i);
+
+				tempStory.setId(result.optString("id"));
+				tempStory.setTitle(result.optString("title"));
+
+				links = result.optJSONObject("links");
+				mobile = links.getJSONObject("mobile");
+				tempStory.setLink(mobile.optString("href"));
+
+				JSONArray images = result.getJSONArray("images");
+
+				if (!images.isNull(0)) {
+					JSONObject url = images.getJSONObject(0);
+					tempStory.setImageUrl(url.getString("url"));
+				} else {
+					tempStory.setImageUrl("");
+				}
+				stories.add(tempStory);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		displayStories();
+	}
 
 	private void displayStories() {
 		adapter = new StoryAdapter(getActivity(), R.layout.story, stories);
@@ -132,7 +251,9 @@ public class MasterFragment extends Fragment {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.refresh:
-			checkNewsFeed();
+			// checkNewsFeed();
+			CheckNews checkNews = new CheckNews();
+			checkNews.execute();
 			return true;
 		case R.id.about:
 			showDialog(getActivity());
@@ -145,10 +266,9 @@ public class MasterFragment extends Fragment {
 	private void showDialog(Context context) {
 		AlertDialog.Builder alert = new AlertDialog.Builder(context);
 		alert.setTitle(context.getResources().getString(R.string.about));
-		WebView wv = new WebView(context);
 
+		WebView wv = new WebView(context);
 		wv.loadUrl("file:///android_asset/credits.html");
-		wv.setHorizontalScrollBarEnabled(false);
 
 		alert.setView(wv);
 		alert.setNegativeButton(context.getResources()
